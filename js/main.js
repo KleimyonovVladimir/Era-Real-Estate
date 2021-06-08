0a1f1dda3883e69f03dc2f92eb88d0b18b548e1a
KleimyonovVladimir <kleimionovvova@gmail.com>
2021-06-08 19:05:40 +0300

    Profitable

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+  $(".profitable__carousel").owlCarousel({
+    loop: true,
+    margin: 27,
+    nav: true,
+    navText: ['<img src="img/left.svg">', '<img src="./img/right.svg">'],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+});
